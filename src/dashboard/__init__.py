7ef9ee873da7c436a66d2c9a4e2586e35e476a6c
Mritunjay-mj <121ec0003@iiitk.ac.in>
2025-03-15 17:51:52 +0530

    Create __init__.py

---
+++ src/dashboard/__init__.py
@@ -0,0 +1,10 @@
+"""
+ASIRA Dashboard Module
+Provides visualization and control interface for security operations
+
+Version: 1.0.0
+Last updated: 2025-03-15 12:21:15
+Last updated by: Mritunjay-mj
+"""
+
+__version__ = "1.0.0"
