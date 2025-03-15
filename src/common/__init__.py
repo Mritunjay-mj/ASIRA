bbe32b8f01923ac376b729b4dbe9811c7b20259e
Mritunjay-mj <121ec0003@iiitk.ac.in>
2025-03-15 17:28:02 +0530

    Create __init__.py

---
+++ src/common/__init__.py
@@ -0,0 +1,11 @@
+"""
+ASIRA Common Module
+
+Contains utility functions, configuration, database connections,
+logging setup, and security functions used throughout the ASIRA system.
+
+Version: 1.0.0
+Last updated: 2025-03-15
+"""
+
+__version__ = "1.0.0"
