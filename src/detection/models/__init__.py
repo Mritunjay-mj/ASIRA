b34d0259fbe4e5bc34fe774498567d81b6dc4e44
Mritunjay-mj <121ec0003@iiitk.ac.in>
2025-03-15 23:51:14 +0530

    Update __init__.py

--- src/detection/models/__init__.py
+++ src/detection/models/__init__.py
@@ -1,9 +1,30 @@
 """
-ASIRA Detection Module
-Implements anomaly detection capabilities using multiple machine learning techniques
+Anomaly detection models for ASIRA
+
+This package contains various anomaly detection models:
+- Statistical methods (Z-score, MAD)
+- Machine learning models (Isolation Forest)
+- Deep learning models (Autoencoder)
+- Ensemble methods
 
 Version: 1.0.0
-Last updated: 2025-03-15
+Last updated: 2025-03-15 18:13:58
+Last updated by: Rahul
 """
 
-__version__ = "1.0.0"
+from src.detection.models.base import BaseModel, ModelConfig
+from src.detection.models.statistical import StatisticalModel
+from src.detection.models.isolation_forest import IsolationForestModel
+from src.detection.models.autoencoder import AutoencoderModel
+from src.detection.models.ensemble import EnsembleModel
+from src.detection.models.registry import ModelRegistry
+
+__all__ = [
+    'BaseModel',
+    'ModelConfig',
+    'StatisticalModel',
+    'IsolationForestModel',
+    'AutoencoderModel',
+    'EnsembleModel',
+    'ModelRegistry'
+]
