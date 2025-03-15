de027645b37196446a8703be13ce1f2238f2494d
Mritunjay-mj <121ec0003@iiitk.ac.in>
2025-03-15 17:45:52 +0530

    Create __init__.py

---
+++ src/response/__init__.py
@@ -0,0 +1,10 @@
+"""
+ASIRA Response Module
+Implements automated security incident response capabilities
+
+Version: 1.0.0
+Last updated: 2025-03-15 12:14:58
+Last updated by: Mritunjay-mj
+"""
+
+__version__ = "1.0.0"
