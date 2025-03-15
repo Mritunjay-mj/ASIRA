c78088d49238d89e01c964172836ea10a999df07
Mritunjay-mj <121ec0003@iiitk.ac.in>
2025-03-15 17:40:10 +0530

    Create __init__.py

---
+++ src/detection/__init__.py
@@ -0,0 +1,9 @@
+"""
+ASIRA Detection Module
+Implements anomaly detection capabilities using multiple machine learning techniques
+
+Version: 1.0.0
+Last updated: 2025-03-15
+"""
+
+__version__ = "1.0.0"
